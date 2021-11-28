a899466d615856d06e4e0a52c6dd95effc505565
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-11-28 12:39:20 +0530

    formatted

--- src/pages/Featured.js
+++ src/pages/Featured.js
@@ -1,20 +1,18 @@
 import AbstractView from "../../.leafjs/static/js/AbstractView.js";
 
 export default class Featured extends AbstractView {
-    constructor(params) {
-        super(params);
-        this.setTitle("Featured");
-    }
+  constructor(params) {
+    super(params);
+    this.setTitle("Featured");
+  }
 
-    async getHTML() {
-        return <>
-            <h1>NEW SONGS:</h1>
-            <p>
-                Phoenix
-            </p>
-            <p>
-                Like This
-            </p>
-        </>;
-    }
+  async getHTML() {
+    return (
+      <>
+        <h1>NEW SONGS:</h1>
+        <p>Phoenix</p>
+        <p>Like This</p>
+      </>
+    );
+  }
 }

a2a441cd7d2df72fe325cf0da96a993e5a4e1467
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-11-28 07:50:51 +0530

    modified console.log in build/server.js

--- build/server.js
+++ build/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const port = process.env.PORT || 25565
+
 const app = express();
 
 app.use("/.leaf_dist", express.static(path.resolve(__dirname, ".leaf_dist")));
@@ -9,4 +11,4 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
-app.listen(process.env.PORT || 25565, () => console.log("Server running..."));
+app.listen(port, () => console.log(`LISTENING PORT ${port}`));

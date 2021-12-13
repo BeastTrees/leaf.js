dbe671280c593272e86ae715fd03aa7ec5bfde95
DasLixou <einlixou@gmail.com>
2021-12-13 14:37:05 +0100

    🗂️ Reorganized Project (monorepo)

---
+++ examples/tailwind-single-page-application/.leafjs/static/js/leafjs.js
@@ -0,0 +1,16 @@
+import Router from "./router.js";
+
+export default class LeafJS {
+  constructor() {
+    this.router = new Router();
+  }
+
+  getRouter() {
+    return this.router;
+  }
+}
+
+const useLeafJS = new LeafJS();
+Object.freeze(useLeafJS);
+
+export { useLeafJS };
